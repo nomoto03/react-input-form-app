6328089b67bb4cce914679681047cf8cb3e344e2
nomoto03 <nekomon6030@gmail.com>
2023-08-01 19:37:47 +0900

    form作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+// react-hook-formから、useFormとSubmitHandlerをimport
+// SubmitHandlerは、submitイベントに関する関数の型宣言に使う
+import { useForm, SubmitHandler } from "react-hook-form";
 
-function App() {
+// フォームの入力値についての型定義。useFormフックを書く時に使う。
+type FormInput = {
+  name: string;
+  age: number;
+};
+
+export default function App() {
+  // useFormフックを使い、registerとhandleSubmitを取得する。
+  // registerは、フォームのフィールドを登録することで、バリデーションを機能させる。
+  // handleSubmitは、submitイベントの制御に関わる。
+  const { register, handleSubmit } = useForm<FormInput>();
+
+  // submitイベントが発生して、かつバリデーションが成功した場合に実行する関数。
+  // サンプルコードなので、入力値（data）をコンソール出力するだけの処理を用意。
+  const onSubmit: SubmitHandler<FormInput> = (data) => console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // handleSubmitの第1引数は、バリデーションを通った場合にのみ実行される。
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        名前
+        {/* registerの第1引数の文字列は、name属性の値としても利用される。 */}
+        {/* registerの第2引数には、バリデーションの種類を設定できる。 */}
+        {/* requiredにtrueを設定すると、入力必須の状態になる。 */}
+        <input {...register("name", { required: true })} />
+      </label>
+      <label>
+        年齢
+        <input type="number" {...register("age", { required: true })} />歳
+      </label>
+      <button>送信する</button>
+    </form>
   );
 }
-
-export default App;
